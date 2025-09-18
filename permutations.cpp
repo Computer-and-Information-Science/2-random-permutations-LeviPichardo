3c856abc08958b18e3d1bcb0b8ee28c59d933333
LeviPichardo <levi.pichardo@student.northampton.edu>
2025-09-18 04:12:26 +0000

    Complete

--- permutations.cpp
+++ permutations.cpp
@@ -1,4 +1,4 @@
-// YOUR NAME HERE
+//Levi P
 
 #include "permutations.h"
 #include "randint.h"
@@ -23,14 +23,40 @@ static size_t search (const int array[], size_t size, int target) {
     return size;
 }
 
-void permutations1 (int array[], size_t size) {
+void permutations1 (int array[], size_t size) {;
     // TODO: Implement algorithm #1 here
+void permutations1(int array[], size_t size) {;
+    size_t count = 0;
+    while (count < size) {;
+        int x = randint(1, static_cast<int>(size));
+        if (search(array, count, x) == count) {
+            array[count] = x;
+            count++;
+        }
+    }
 }
 
-void permutations2 (int array[], size_t size) {
-    // TODO: Implement algorithm #2 here
+}
+
+void permutations2 (int array[], size_t size) 
+    // TODO: Implement algorithm #2 here;
+    void permutations2(int array[], size_t size) {;
+    bool* used = new bool[size + 1](); 
+
+    for (size_t i = 0; i < size; ) {
+        int x = randint(1, static_cast<int>(size));
+        if (!used[x]) {
+            used[x] = true;
+            array[i++] = x;
+        }
+    }
 }
 
 void permutations3 (int array[], size_t size) {
     // TODO: Implement algorithm #3 here
+    void permutations3(int array[], size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        array[i] = static_cast<int>(i + 1);
+    }
 }
+//was a bit stuck
